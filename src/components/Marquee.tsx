b068ed6c57cb164741dd1f203eb6ca95b071b6a2
Vuthuru Srivardhan <vuthurusrivardhan@gmail.com>
2025-04-25 13:59:56 +0530

    fixed the marquee animation

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -1,33 +1,23 @@
 export default function Marquee(): React.ReactElement {
     const names = [
-        {
-            name: "Indus Towers Ltd.",
-        },
-        {
-            name: "Cloud Excel Pvt. Ltd.",
-        },
-        {
-            name: "Crest Digital India Pvt. Ltd.",
-        },
-        {
-            name: "Shaurrya Teleservices Pvt. Ltd.",
-        },
-        {
-            name: "FN Global Pvt. Ltd.",
-        },
-    ]
+        { name: "Indus Towers Ltd." },
+        { name: "Cloud Excel Pvt. Ltd." },
+        { name: "Crest Digital India Pvt. Ltd." },
+        { name: "Shaurrya Teleservices Pvt. Ltd." },
+        { name: "FN Global Pvt. Ltd." },
+    ];
 
     return (
-        <div>
-            <marquee>
-                <div className="flex gap-32">
-                    {names.map(function (name, key) {
-                        return (<div key={key}>
-                            <div className="rounded-sm py-2.5 px-5 text-center font-semibold text-md bg-primary-blue-100 text-primary-white-100">{name.name}</div>
-                        </div>);
-                    })}
-                </div>
-            </marquee>
-        </div >
+        <div className="overflow-hidden whitespace-nowrap">
+            <div className="animate-marquee flex gap-32">
+                {[...names, ...names].map((name, key) => (
+                    <div key={key}>
+                        <div className="rounded-sm py-2.5 px-5 text-center font-semibold text-md bg-primary-blue-100 text-primary-white-100">
+                            {name.name}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 }
