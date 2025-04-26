8b96e5f7ca63309b2849bd30d8939076badc8386
Vuthuru Srivardhan <vuthurusrivardhan@gmail.com>
2025-04-26 17:49:18 +0530

    removed the 404 error

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,47 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import AboutClients from './components/AboutClients'
-import Footer from './components/Footer'
-import Home from './components/Home'
-import OurStrength from './components/OurStrength'
-import Services from './components/Services'
-import Navbar from './components/Navbar'
-import OurProjects from './components/OurProjects'
-import OandM from './components/OandM'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AboutClients from './components/AboutClients';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import OurStrength from './components/OurStrength';
+import Services from './components/Services';
+import Navbar from './components/Navbar';
+import OurProjects from './components/OurProjects';
+import OandM from './components/OandM';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={
-          <div className='w-full h-screen'>
-            <Home />
-            <AboutClients />
-            <Services />
-            <OurStrength />
-            <Footer />
-          </div>
-        } />
-        <Route path="/ourprojects" element={
-          <div className='w-full h-screen'>
-            <Navbar />
-            <OurProjects />
-            <Footer />
-          </div>
-        } />
-        <Route path="/oandm" element={
-          <div className='w-full h-screen'>
-            <Navbar />
-            <OandM />
-            <Footer />
-          </div>
-        } />
-      </Routes>
+      <div className="w-full min-h-screen flex flex-col">
+        {/* Navbar shown only on certain pages */}
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Home />
+              <AboutClients />
+              <Services />
+              <OurStrength />
+              <Footer />
+            </>
+          } />
+          <Route path="/ourprojects" element={
+            <>
+              <Navbar />
+              <OurProjects />
+              <Footer />
+            </>
+          } />
+          <Route path="/oandm" element={
+            <>
+              <Navbar />
+              <OandM />
+              <Footer />
+            </>
+          } />
+        </Routes>
+      </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
